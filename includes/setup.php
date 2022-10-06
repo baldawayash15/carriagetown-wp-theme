8a6dd63c4c7a9b17af839ba65072d3e40edf9de8
Yash Baldawa <baldawayash15@gmail.com>
2022-10-06 19:31:01 +0530

    Adding HTML5 Support

--- includes/setup.php
+++ includes/setup.php
@@ -3,6 +3,7 @@
 function cc_setup_theme() {
     add_theme_support( 'title-tag' );
     add_theme_support( 'custom-logo' );
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
 
     register_nav_menu( 'primary-menu', __('Primary Menu', 'carriagetown') );
     register_nav_menu( 'footer-menu', __( 'Footer Menu', 'carriagetown' ) );
